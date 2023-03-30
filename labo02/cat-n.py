849b0c8b2bf9680c203b85f1e0435fdc285b9a9f
sebastien <s122861@ap.be>
2023-03-30 12:48:11 +0200

    Added exception handling

--- labo02/cat-n.py
+++ labo02/cat-n.py
@@ -5,11 +5,18 @@ import sys
 data = sys.argv[1:]
 line_number = 1
 
-for filename in data:
-    with open(filename, "r") as file:
-        for line in file:
-            formatted_line_number = str(line_number).zfill(6)
-            print(f"{formatted_line_number}\t{line}", end="")
-            line_number += 1
+try:
+    for filename in data:
+        with open(filename, "r") as file:
+            for line in file:
+                formatted_line_number = str(line_number).zfill(6)
+                print(f"{formatted_line_number}\t{line}", end="")
+                line_number += 1
 
-    print()
+        print()
+
+except FileNotFoundError:
+    print("Er is een bestand niet gevonden. Controleer de opgegeven bestandsnamen en probeer het opnieuw.")
+
+except Exception as e:
+    print("Er is een onverwachte fout opgetreden:", e)
